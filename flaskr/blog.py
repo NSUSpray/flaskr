1616803021b188bd0402d1ad45b365c0cd98c2f3
NSUSpray <nsu.spray@gmail.com>
2023-07-13 12:28:06 +0600

    Add commenting a post

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -17,8 +17,10 @@ def index():
         ORDER BY created DESC
     ''').fetchall()
     reactions = [get_reactions(post['id']) for post in posts]
+    comments = [get_comments(post['id']) for post in posts]
     return render_template(
-        'blog/index.html.jinja', posts=posts, reactions=reactions
+        'blog/index.html.jinja',
+        posts=posts, reactions=reactions, comments=comments
     )
 
 
@@ -66,8 +68,7 @@ def get_post(id, check_author=True):
 
 
 def get_reactions(post_id):
-    db = get_db()
-    reactions = db.execute('''
+    reactions = get_db().execute('''
         SELECT user_id
         FROM reaction r
         WHERE post_id = ?''',
@@ -76,12 +77,42 @@ def get_reactions(post_id):
     return [reaction['user_id'] for reaction in reactions]
 
 
+def get_comments(post_id):
+    comments = get_db().execute('''
+        SELECT c.id, body, created, author_id, username
+        FROM comment c JOIN user u ON c.author_id = u.id
+        WHERE post_id = ?
+        ORDER BY created DESC''',
+        (post_id,)
+    ).fetchall()
+    return comments
+
+
+def get_comment(id, check_author=True):
+    comment = get_db().execute('''
+        SELECT c.id, body, created, author_id, post_id, username
+        FROM comment c JOIN user u ON c.author_id = u.id
+        WHERE c.id = ?''',
+        (id,)
+    ).fetchone()
+
+    if comment is None:
+        abort(404, f"Comment id {id} doesn't exist.")
+
+    if check_author and comment['author_id'] != g.user['id']:
+        abort(403)
+
+    return comment
+
+
 @bp.route('/<int:id>')
 def read(id):
     post = get_post(id, check_author=False)
     reactions = get_reactions(post['id'])
+    comments = get_comments(post['id'])
     return render_template(
-        'blog/read.html.jinja', post=post, reactions=reactions
+        'blog/read.html.jinja',
+        post=post, reactions=reactions, comments=comments
     )
 
 
@@ -143,3 +174,35 @@ def like(id):
         )
     db.commit()
     return redirect(url_for('blog.read', id=id))
+
+
+@bp.route('/<int:id>/comment', methods=('POST',))
+def comment(id):
+    body = request.form['body']
+    error = None
+
+    if not body:
+        error = 'Message is required.'
+
+    if error is not None:
+        flash(error)
+    else:
+        db = get_db()
+        db.execute('''
+            INSERT INTO comment (body, author_id, post_id)
+            VALUES (?, ?, ?)''',
+            (body, g.user['id'], id)
+        )
+        db.commit()
+
+    return redirect(url_for('blog.read', id=id))
+
+
+@bp.route('/delete_comment/<int:id>', methods=('POST',))
+@login_required
+def delete_comment(id):
+    comment = get_comment(id)
+    db = get_db()
+    db.execute('DELETE FROM comment WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.read', id=comment['post_id']))
